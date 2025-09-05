371f541f75a782e73c6bced22a8b392c14811fa9
Ansul Gupta <ansugupt@adobe.com>
2025-09-05 21:22:04 +0530

    Update app.py to use environment variable for port configuration

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template
+import os
 
 app = Flask(__name__)
 
@@ -7,4 +8,5 @@ def index():
     return render_template("index.html")  # Single-page UI
 
 if __name__ == "__main__":
-    app.run(debug=True,host="0.0.0.0",port=5000)
+    port = int(os.environ.get('PORT', 8000))
+    app.run(debug=True,host="0.0.0.0",port=port)
